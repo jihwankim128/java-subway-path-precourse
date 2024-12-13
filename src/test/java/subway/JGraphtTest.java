1f6a0d80fea232785550fbded72fde196601e878
jihwan <jihwan1535@nate.com>
2024-12-14 07:29:36 +0900

    test: JGraph 테스트 코드 추가

--- src/test/java/subway/JGraphtTest.java
+++ src/test/java/subway/JGraphtTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import org.assertj.core.api.Assertions;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -25,4 +26,37 @@ public class JGraphtTest {
 
         assertThat(shortestPath.size()).isEqualTo(3);
     }
+
+    @Test
+    void 연결되어있지_않은_간선_정보로_경로를_구하면_예외가_발생한다() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");  graph.addVertex("v2"); graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 2);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        Assertions.assertThatThrownBy(() -> dijkstraShortestPath.getPath("v2", "v3").getVertexList())
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void 없는_정점_번호로_간선을_만들_경우_예외가_발생한다() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");  graph.addVertex("v2"); graph.addVertex("v3");
+        Assertions.assertThatThrownBy(() -> graph.addEdge("v3", "v4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 연결되어_있지_않은_경로를_구할_경우_예외가_발생한다() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");  graph.addVertex("v2"); graph.addVertex("v3"); graph.addVertex("v4");
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v4"), 2);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        Assertions.assertThatThrownBy(() -> dijkstraShortestPath.getPath("v1", "v4").getVertexList())
+                .isInstanceOf(NullPointerException.class);
+    }
+
 }
